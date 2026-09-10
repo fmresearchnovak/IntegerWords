328ef9d67712213abb1e325f683459e35cd5adcf
Prof. Novak <enovak@fandm.edu>
2026-09-10 15:18:42 -0400

    .

--- src/integerwords/cli.py
+++ src/integerwords/cli.py
@@ -1,14 +1,20 @@
 
+import os
 import sys
 
 from .IntegerWords import EnglishInteger
 
 def main():
+	prog = os.path.basename(sys.argv[0]) or "intw"
+
 	if(len(sys.argv) != 2):
-		print("Usage: intw <number>")
+		print("Usage: " + prog + " <number>")
 		sys.exit(1)
 
 	user_input = sys.argv[1]
 	user_input = user_input.replace(",", "")
 	num = int(user_input)
 	print(EnglishInteger(num))
+	
+if __name__ == "__main__":
+	main()
